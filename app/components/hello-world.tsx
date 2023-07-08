9211ee1bf6374c65eff7013fb1a6e82e61ff9412
rohan3011 <rohankamble3011@gmail.com>
2023-07-08 13:04:08 +0530

    Added shadcn

---
+++ app/components/hello-world.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "~/components/ui/button";
+
+export default function HelloWorld() {
+  return (
+    <div>
+      HelloWorld
+      <Button>Click Me</Button>
+    </div>
+  );
+}
