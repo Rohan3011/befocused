5293559e1b60dbbe904369deb106c4bc7a0dfb6f
rohan3011 <rohankamble3011@gmail.com>
2023-07-14 19:46:38 +0530

    Fixed folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HelloWorld from "~/components/hello-world";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen mx-auto max-w-2xl flex-col items-center justify-between bg-blue-500 p-2 md:p-4 lg:p-10">
       <HelloWorld />
     </main>
   );
